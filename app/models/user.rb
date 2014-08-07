d4f6f420a118b172c2074362907161bc6cb22a7d
qwlong <wenlongqi@gmail.com>
2014-08-08 01:55:13 +0800

    单点登录功能开发

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,27 @@ class User < ActiveRecord::Base
   scope :locked, -> { where.not(locked_at: nil) }
   scope :top, ->(n) { order(comments_count: :desc, topics_count: :desc).limit(n) }
 
+  after_create :syn_info_to_sso
+  before_save  :syn_confirmed_at_to_sso,
+
+  def syn_info_to_sso
+    cu = CasUser.create_with(self.attributes.keep_if{ |k, v| (CasUser.attribute_names - ['id']).include?(k) }).find_or_initialize_by(email: email)
+    cu.encrypted_password = devise_password_digest(self.password) if cu.encrypted_password.blank? or cu.crypted_password.blank?
+    cu.save!
+  end
+
+  def syn_confirmed_at_to_sso
+    if confirmed? or username_changed? or email_changed? or mobile_changed?
+      cu = CasUser.find_by(email: email)
+      if cu.present?
+        cu.confirmed_at = Time.now if confirmed_changed?
+        cu.username = username if username_changed?
+        cu.mobile = mobile if mobile_changed?
+        cu.save!
+      end
+    end
+  end
+
   def remember_token
     [id, Digest::SHA512.hexdigest(password_digest)].join('$')
   end
@@ -80,4 +101,10 @@ class User < ActiveRecord::Base
   rescue ActiveSupport::MessageVerifier::InvalidSignature
     nil
   end
+
+  protected
+  def devise_password_digest(password)
+    ::BCrypt::Password.create("#{password}#{nil}", cost: 10).to_s
+  end
+
 end
