c06ec1b308ccccb378648ca2d6ef239d0cf54c7d
qwlong <wenlongqi@gmail.com>
2014-08-12 23:42:28 +0800

    单点登录功能开发

---
+++ app/uploaders/adfile_uploader.rb
@@ -0,0 +1,21 @@
+class AvatarUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  version :small do
+    process resize_to_fill: [160, 80]
+  end
+
+  version :thumb do
+    process resize_to_fill: [40, 20]
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+end
